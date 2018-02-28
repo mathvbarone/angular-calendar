587e652f456f3766c35240527b0fde0cb85cde17
Matheus Barone <matheusvbarone@gmail.com>
2018-02-27 22:04:58 -0300

    Estruturando o projeto

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'app';
+
+  public date = moment();
+
+  public ngOnInit() {
+  }
+
+
+}

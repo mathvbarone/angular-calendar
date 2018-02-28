3ae2ab75c42d58bca8ecd0b3bda5a88408610392
Matheus Barone <matheusvbarone@gmail.com>
2018-02-27 23:43:20 -0300

    Calendario dinamico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,13 +11,21 @@ export class AppComponent implements OnInit {
 
   public daysArr;
 
+  public ngOnInit() {
+    this.daysArr = this.createCalendar(this.date);
+  }
+
   createCalendar(month) {
     const firstDay = moment(month).startOf('M');
-    const days = Array.apply(null, { length: month.daysInMonth()})
-      .map(Number.call, Number)
-      .map( n => {
-        return moment( firstDay ).add( n, 'd' );
-      });
+
+    const days = Array(month.daysInMonth()).fill(null)
+    .map((el, i) => i)
+    .map( n => {
+      return moment( firstDay ).add( n, 'd' );
+    });
+
+    // const days = Array.apply(null, { length: month.daysInMonth()})
+    //   .map(Number.call, Number)
 
        for ( let n = 0; n < firstDay.weekday(); n++ ) {
          days.unshift(null);
@@ -34,8 +42,16 @@ export class AppComponent implements OnInit {
     return moment().format('L') === day.format('L');
   }
 
-
-  public ngOnInit() {
+  nextMonth() {
+    this.date.add(1, 'M');
     this.daysArr = this.createCalendar(this.date);
   }
+
+  previousMonth() {
+    this.date.subtract(1, 'M');
+    this.daysArr = this.createCalendar(this.date);
+  }
+
+
+
 }
